f25167bfa7bc0aad02b6fb91d3c4aaffdb7b49e4
TinoMaster <oscarmarcos2673@gmail.com>
2023-10-15 11:12:43 +0200

    ok

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -3,6 +3,8 @@ import { BlogCategory } from "../models/types/Blog";
 
 export class BlogService {
   static async getBlogs(category: BlogCategory) {
+    /* const blogsToReturn = [] */
+
     return blogs.filter(
       (el) => el.category === category || category === "general"
     );
