cc9f2fc5f28b7887fe5ef0b76b4005f9e9a6884d
TinoMaster <oscarmarcos2673@gmail.com>
2023-10-23 15:58:06 +0200

    ok

--- src/Components/global/Hero/ImageHero.tsx
+++ src/Components/global/Hero/ImageHero.tsx
@@ -1,8 +1,10 @@
+import { LazyImage } from "../LazyImage";
+
 export const ImageHero = ({ image }: { image: string }) => {
   return (
     <div className="w-full md:w-1/2 flex justify-center md:justify-end">
       <div className="w-60 h-60 xl:w-80 xl:h-80 rounded-full overflow-hidden border-2">
-        <img src={image} alt="my_picture" className="w-full h-full object-cover" />
+        <LazyImage src={image} alt="" />
       </div>
     </div>
   );
