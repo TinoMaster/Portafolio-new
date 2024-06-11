9f3069c2788c29051134f0f9408ea39710bd4f5e
TinoMaster <oscarmarcos2673@gmail.com>
2024-06-11 21:46:26 +0200

    last commit

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -1,22 +1,22 @@
-import { server } from "../config/server.config";
-import { Blog, BlogCategory } from "../models/types/Blog";
-import { getAllBlogs, getBlogByCategory, getBlogById } from "./blogFunction";
+import { Blog, BlogCategory } from '../models/types/Blog'
+import { getAllBlogs, getBlogByCategory, getBlogById } from './blogFunction'
 
 export class BlogService {
-  static url = server.production;
+   /* static url = server.production; */
+   static url = 'https://debts-api-dev-rtxq.2.ie-1.fl0.io/api/v1/blogs'
 
-  static async getBlogs(category: BlogCategory | undefined) {
-    if (category) {
-      const res = await getBlogByCategory(this.url, category);
-      return res;
-    } else {
-      const res = await getAllBlogs(this.url);
-      return res;
-    }
-  }
+   static async getBlogs(category: BlogCategory | undefined) {
+      if (category) {
+         const res = await getBlogByCategory(this.url, category)
+         return res
+      } else {
+         const res = await getAllBlogs(this.url)
+         return res
+      }
+   }
 
-  static async getItemBlog(id: string): Promise<Blog> {
-    const res = await getBlogById(this.url, id);
-    return res;
-  }
+   static async getItemBlog(id: string): Promise<Blog> {
+      const res = await getBlogById(this.url, id)
+      return res
+   }
 }
